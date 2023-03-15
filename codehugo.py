6c651501e2106f43cca7a2078e0f38df0bf0bd1c
HuCSo <127121275+HuCSo@users.noreply.github.com>
2023-03-15 15:46:35 +0000

    Update Hugo

--- codehugo.py
+++ codehugo.py
@@ -2,18 +2,22 @@ import torch
 import torchvision
 import torchvision.transforms as transforms
 import math
-from base import *
+from base import get_mocap_rot
 import torch.nn as nn
-train_data=get_mocap_rot('IEMOCAP_full_release_withoutVideos_sentenceOnly/IEMOCAP_full_release/Session1/sentences/MOCAP_rotated/Ses01F_impro01/Ses01F_impro01_F000.txt')[2]
+train_data = get_mocap_rot(
+    '/workspaces/reconaissance_emotions/IEMOCAP_full_release_withoutVideos_sentenceOnly/IEMOCAP_full_release/Session1/sentences/MOCAP_rotated/Ses01F_impro01/Ses01F_impro01_F000.txt')[2]
 train_data_tensor = torch.Tensor(train_data)
-train_data_tensor_normal = torch.nn.functional.normalize(train_data_tensor)
-print(train_data_tensor_normal)
-#val_data_tensor = torch.Tensor(val_data)
-#test_data_tensor = torch.Tensor(test_data)
+data_mean = torch.mean(train_data_tensor)
+data_std = torch.std(train_data_tensor)
+normalized_data = (train_data_tensor - data_mean) / data_std
+print(normalized_data)
+# print(train_data)
+# val_data_tensor = torch.Tensor(val_data)
+# test_data_tensor = torch.Tensor(test_data)
 
-#train_labels_tensor = torch.Tensor(train_labels).long()
-#val_labels_tensor = torch.Tensor(val_labels).long()
-#test_labels_tensor = torch.Tensor(test_labels).long()
+# train_labels_tensor = torch.Tensor(train_labels).long()
+# val_labels_tensor = torch.Tensor(val_labels).long()
+# test_labels_tensor = torch.Tensor(test_labels).long()
 """""
 def preprocess_motion_capture_data(data):
     # Convertir les données en tensor PyTorch
@@ -30,8 +34,8 @@ def preprocess_motion_capture_data(data):
     return normalized_data
 h,xyz,datas=get_mocap_rot('IEMOCAP_full_release_withoutVideos_sentenceOnly/IEMOCAP_full_release/Session1/sentences/MOCAP_rotated/Ses01F_impro01/Ses01F_impro01_F000.txt')
 """
-#print(preprocess_motion_capture_data(datas[:]))
-
+# print(preprocess_motion_capture_data(datas[:]))
+"""""
 
 class RNN(nn.Module):
     def __init__(self, input_size, hidden_size, output_size):
@@ -52,7 +56,7 @@ class RNN(nn.Module):
 
     def initHidden(self):
         return torch.zeros(1, self.hidden_size)
-""""
+
 n_hidden = 128
 rnn = RNN(n_letters, n_hidden, n_categories)
 """
--- dataset_setup.py
+++ dataset_setup.py
@@ -1,3 +1,4 @@
+import os
 import torch
 from torch.utils.data import Dataset
 from torchvision import datasets
@@ -5,7 +6,6 @@ from torchvision.transforms import ToTensor
 import matplotlib.pyplot as plt
 import pandas as pd
 import base
-import os
 from torch.utils.data import DataLoader
 
 #train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
@@ -25,11 +25,15 @@ class IEMOCAP_dataset(Dataset) :
 
     def __len__(self) : 
         return len(self.labels)
-    
+# pour l'instant on normalise (x-mean)/std sur la frame entière
     def __getitem__(self, idx) : 
         item = base.get_mocap_rot(self.data_paths[idx])[2]
         label = self.labels[idx]
-        return torch.from_numpy(item), label
+        item_tensor = torch.Tensor(item)
+        data_mean = torch.mean(item_tensor)
+        data_std = torch.std(item_tensor)
+        normalized_data = (item_tensor - data_mean) / data_std
+        return normalized_data, label
     
 
 
@@ -37,7 +41,7 @@ class IEMOCAP_dataset(Dataset) :
 root_path = 'IEMOCAP_full_release_withoutVideos_sentenceOnly'
 info_path = os.path.join(root_path, 'iemocap.csv')
 
-iemocap_dataset = IEMOCAP_dataset(info_path)
+iemocap_dataset = IEMOCAP_dataset(info_path,True)
 
 print(iemocap_dataset.__len__())
 
