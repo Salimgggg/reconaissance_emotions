75f9d60d9375f3689cba7316f899354e76ea69c0
salim_gazzeh <salim.gazzeh@student-cs.fr>
2023-03-22 07:49:29 +0000

    'same'

--- dataset_setup.py
+++ dataset_setup.py
@@ -9,32 +9,39 @@ import base
 from torch.utils.data import DataLoader
 import numpy as np
 
-# train_dataloader = DataLoader(training_data, batch_size=64, shuffle=True)
-# test_dataloader = DataLoader(test_data, batch_size=64, shuffle=True)
+
 
 
 class IEMOCAP_dataset(Dataset):
 
-    def __init__(self, info_path, test):
-        self.info = pd.read_csv(info_path)  # this is the df from base
-        if test == True:
-            self.data_paths = self.info['MOCAP_rotated_path'][:7870]
+    def __init__(self, info_path, training):
+        self.info_path = info_path
+        self.training = training
+
+        self.dataframe = pd.read_csv(self.info_path)  # this is the df from base
+
+        if self.training :
+            self.data_paths = self.dataframe['MOCAP_rotated_path'][:7870]
+            self.labels = self.dataframe['emotion'][:7870]
         else:
-            self.data_paths = self.info['MOCAP_rotated_path'][7870:]
-        self.labels = self.info['emotion']
+            self.data_paths = self.dataframe['MOCAP_rotated_path'][7870:].reset_index(drop = True)
+            self.labels = self.dataframe['emotion'][7870:].reset_index(drop = True)
+
 
     def __len__(self):
-        return len(self.labels)
+            return len(self.labels)
 # pour l'instant on normalise (x-mean)/std sur la frame entière
 
-    def __getitem__(self, idx):
+    def __getitem__(self, idx): 
         item = base.get_mocap_rot(self.data_paths[idx])[2]
         label = self.labels[idx]
-        data_mean = np.mean(item, 0)
-        data_std = np.std(item, 0)
-        normalized_data = (item - data_mean)/data_mean
-        item_tensor = torch.Tensor(item)
-        return normalized_data, label
+        
+        # data_mean = np.mean(item, 0)
+        # data_std = np.std(item, 0)
+        # normalized_data = (item - data_mean)/data_mean
+        # item_tensor = torch.Tensor(item)
+        # return normalized_data, label
+        return item, label
 
 
 root_path = 'IEMOCAP_full_release_withoutVideos_sentenceOnly'
@@ -42,8 +49,14 @@ info_path = os.path.join(root_path, 'iemocap.csv')
 
 iemocap_dataset = IEMOCAP_dataset(info_path, True)
 
-print(iemocap_dataset.__len__())
+print(len(iemocap_dataset))
+print(iemocap_dataset[0])
 
-example = iemocap_dataset.__getitem__(4)
+training_data = IEMOCAP_dataset(info_path, True)
+test_data = IEMOCAP_dataset(info_path, False)
 
-print(example)
+
+train_dataloader = DataLoader(training_data, batch_size=64, shuffle=False)
+test_dataloader = DataLoader(test_data, batch_size=64, shuffle=False)
+
+print(next(iter(test_dataloader)))
